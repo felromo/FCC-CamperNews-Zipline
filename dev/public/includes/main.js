03fd400d84fc8b8ea468c09fb5ceb8912e86a89c
Tyrantz <raymanv3@gmail.com>
2016-01-05 03:56:37 +0000

    Implements new responsive grid layout

--- dev/public/includes/main.js
+++ dev/public/includes/main.js
@@ -1,10 +1,46 @@
-var camperNewsApp = angular.module('newsApp', [])
+angular.module('newsApp', ['angularGrid'])
 
-.factory('newsFactory', function () {
-  var news = {};
-  return news;
-})
+.service('imageService', ['$http', function ($http) {
+  this.loadImages = function () {
+    return $http.get('http://www.freecodecamp.com/news/hot');
+  };
+}])
 
-.controller('newsController',['newsFactory', function (newsFactory) {
+.controller('newsController', ['$scope', 'imageService', 'angularGridInstance', function ($scope, imageService, angularGridInstance) {
+  self = this;
+  // self.pics;
+
+  self.imageValues = function (image) {
+    image.addEventListener('onload', function () {
+      console.log("width is: " + this.naturalWidth);
+      console.log("height is: " + this.naturalHeight);
+    });
+  };
+
+  $scope.refresh = function () {
+    angularGridInstance.gallery.refresh();
+  };
+
+  self.init = function () {
+    imageService.loadImages().then(function (reply) {
+      var images_rough = reply.data;
+      var all_images = [];
+
+      images_rough.forEach(function (value) {
+        if (value.image.length === 0) {
+          all_images.push(value.author.picture);
+        } else if (value.image == "undefined") {
+          all_images.push(value.author.picture);
+        } else {
+          all_images.push(value.image);
+        }
+      });
+
+      $scope.pics = all_images;
+
+    });
+  };
+
+  self.init();
 
 }]);
