c268b9f679a05018ffcd040809b60b625c3237d4
Tyrantz <raymanv3@gmail.com>
2016-01-05 10:25:00 +0000

    removes use of $scope

--- dev/public/includes/main.js
+++ dev/public/includes/main.js
@@ -6,8 +6,9 @@ angular.module('newsApp', ['angularGrid'])
   };
 }])
 
-.controller('newsController', ['$scope', 'imageService', 'angularGridInstance', function ($scope, imageService, angularGridInstance) {
+.controller('newsController', ['imageService', 'angularGridInstance', function (imageService, angularGridInstance) {
   self = this;
+  self.articles = [];
 
   self.imageValues = function (image) {
     image.addEventListener('onload', function () {
@@ -23,22 +24,17 @@ angular.module('newsApp', ['angularGrid'])
   self.init = function () {
     imageService.loadImages().then(function (reply) {
       var rough_articles = reply.data;
-      // var all_images = [];
       // these will be the final articles already processed with the necessary information
       var processing_articles = [];
-      $scope.articles = [];
 
       // some images have either empty image strings or "undefined" strings
       rough_articles.forEach(function (value) {
         var image = null;
         if (value.image.length === 0) {
-          // all_images.push(value.author.picture);
           image = value.author.picture;
         } else if (value.image == "undefined") {
-          // all_images.push(value.author.picture);
           image = value.author.picture
         } else {
-          // all_images.push(value.image);
           image = value.image;
         }
         processing_articles.push({
@@ -48,8 +44,7 @@ angular.module('newsApp', ['angularGrid'])
         });
       });
 
-      // $scope.pics = all_images;
-      $scope.articles = processing_articles;
+      self.articles = processing_articles;
 
     });
   };
