918c16a45e451041814d8cb3d0b25b04f10efbe0
Tyrantz <raymanv3@gmail.com>
2016-01-05 10:16:49 +0000

    adds title, and upvotes

--- dev/public/includes/main.js
+++ dev/public/includes/main.js
@@ -22,20 +22,34 @@ angular.module('newsApp', ['angularGrid'])
 
   self.init = function () {
     imageService.loadImages().then(function (reply) {
-      var images_rough = reply.data;
+      var rough_articles = reply.data;
       var all_images = [];
+      // these will be the final articles already processed with the necessary information
+      var processing_articles = [];
+      $scope.articles = [];
 
-      images_rough.forEach(function (value) {
+      // some images have either empty image strings or "undefined" strings
+      rough_articles.forEach(function (value) {
+        var image = null;
         if (value.image.length === 0) {
           all_images.push(value.author.picture);
+          image = value.author.picture;
         } else if (value.image == "undefined") {
           all_images.push(value.author.picture);
+          image = value.author.picture
         } else {
           all_images.push(value.image);
+          image = value.image;
         }
+        processing_articles.push({
+          'image': image,
+          'title': value.headline,
+          'upvotes': value.rank,
+        });
       });
 
       $scope.pics = all_images;
+      $scope.articles = processing_articles;
 
     });
   };
