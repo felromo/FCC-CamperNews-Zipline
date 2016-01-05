a6704e5bc26a731e8d92a622deb255adf2480218
Tyrantz <raymanv3@gmail.com>
2016-01-05 10:19:50 +0000

    changes to article objects

--- dev/public/includes/main.js
+++ dev/public/includes/main.js
@@ -23,7 +23,7 @@ angular.module('newsApp', ['angularGrid'])
   self.init = function () {
     imageService.loadImages().then(function (reply) {
       var rough_articles = reply.data;
-      var all_images = [];
+      // var all_images = [];
       // these will be the final articles already processed with the necessary information
       var processing_articles = [];
       $scope.articles = [];
@@ -32,13 +32,13 @@ angular.module('newsApp', ['angularGrid'])
       rough_articles.forEach(function (value) {
         var image = null;
         if (value.image.length === 0) {
-          all_images.push(value.author.picture);
+          // all_images.push(value.author.picture);
           image = value.author.picture;
         } else if (value.image == "undefined") {
-          all_images.push(value.author.picture);
+          // all_images.push(value.author.picture);
           image = value.author.picture
         } else {
-          all_images.push(value.image);
+          // all_images.push(value.image);
           image = value.image;
         }
         processing_articles.push({
@@ -48,7 +48,7 @@ angular.module('newsApp', ['angularGrid'])
         });
       });
 
-      $scope.pics = all_images;
+      // $scope.pics = all_images;
       $scope.articles = processing_articles;
 
     });
